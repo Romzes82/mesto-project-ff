65571734986ac0c9165c152e03bab19edd948f9f
Roman Maslennikov <romzes120282@gmail.com>
2024-01-28 21:06:12 +0300

    Function addCard is done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function addCard(src, alt, deleteCardFunc) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция удаления карточки
+    // наполняем содержимым
+    cardElement.querySelector('.card__image').src = src;
+    cardElement.querySelector('.card__image').alt = alt;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCardFunc);
+    cardElement.querySelector('.card__title').textContent = alt;
 
-// @todo: Вывести карточки на страницу
+    // возвращаем DOM-элемент созданной карточки
+    return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCardFunc(evt) {
+    evt.target.parentElement.remove();
+}
+
+// Вывести карточки из массива initialCards на страницу
+initialCards.forEach(element => {
+    placesList.append(addCard(element.link, element.name, deleteCardFunc));
+});
