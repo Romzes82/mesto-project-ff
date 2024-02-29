9ab557189de04508f55f48134a3c5ee559fb846a
Roman Maslennikov <romzes120282@gmail.com>
2024-02-29 16:35:58 +0300

    Done

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -1,51 +1,40 @@
 // Ф-ция открытия модального окна
 export function openModal(popap) { 
-    console.log('in openModal');
-    console.log(popap);
     popap.classList.add('popup_is-opened');
-    // popup.addEventListener('click', removeClassPoppupIsOpened_forImage);
-    // popap.classList.toggle('popup_is-opened');
     closePopupByEsc(popap);
-    popap.addEventListener('click', removeClassPoppupIsOpened_forImage);
+    popap.addEventListener('click', closePopupByCrossClick);
+    popap.addEventListener('click', closePopupByOverleyClick); 
 }
 
 // Ф-ция закрытия модального окна
 export function closeModal(popap) {
-    console.log('in closeModal');
-    console.log(popap);
     popap.classList.remove('popup_is-opened');
-    popap.removeEventListener('click', removeClassPoppupIsOpened_forImage);
+    popap.removeEventListener('click', closePopupByCrossClick);
+    popap.removeEventListener('click', closePopupByOverleyClick);
 }
 
-// Функция-обработчик нажатия Esc
-function closePopupByEsc(el) {
-    console.log('in closePopupByEsc');
-    console.log("el:");
-    console.log(el);
+// Функция-обертка (с параметром принимающим попап) для функии-обработчика нажатия Esc
+function closePopupByEsc(popapMustBeClosed) {
     document.addEventListener('keydown', innerClosePopupByEsc);
-
     function innerClosePopupByEsc(evt) {
-        console.log('Я возникаю, когда печатают в текстовом поле.');
-        console.log(evt.key);
         if (evt.key === "Escape") {
-            console.log(el);
-            el.classList.remove('popup_is-opened');
+            // el.classList.remove('popup_is-opened');
+            closeModal(popapMustBeClosed);
             document.removeEventListener('keydown', innerClosePopupByEsc);
         };
     }
 }
 
-// Функция-обработчик нажатия клика по оверлею и крестику
-function removeClassPoppupIsOpened_forImage(evt) {
-    console.log("in removeClassPoppupIsOpened_forImage");
-    console.log("evt.target.classList.value:");
-    console.log(evt.target.classList.value);
-
-    if (evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup_is-opened')) {
-        console.log(evt.currentTarget);
-        console.log(evt.target.classList.value);
-        // evt.currentTarget.classList.remove('popup_is-opened');
+// Функция-обработчик клика по крестику
+function closePopupByCrossClick(evt) {
+    if (evt.target.classList.contains('popup__close')) {
         closeModal(evt.currentTarget);
-        // popupTypeImage.removeEventListener('click', removeClassPoppupIsOpened_forImage);
+    }
+}
+
+// Функция-обработчик клика по оверлею 
+function closePopupByOverleyClick(evt) {
+    if (evt.target.classList.contains('popup_is-opened')) {
+        closeModal(evt.currentTarget);
     }
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,6 @@ import { addCard, deleteCardFunc, likeCardFunc } from './components/card.js';
 import { initialCards } from './cards.js'; 
 import { openModal, closeModal } from './components/modal.js'; 
 
-
-
-// Темплейт карточки
-// const cardTemplate = document.querySelector('#card-template').content;
-
 // DOM узлы
 const placesList = document.querySelector('.places__list');
 // DOM узел попапа edit
@@ -20,101 +15,72 @@ const popupTypeImage = document.querySelector('.popup_type_image');
 const profileEditButton = document.querySelector('.profile__edit-button');
 //дом узел кнопки добавления профиля
 const profileAddButton = document.querySelector('.profile__add-button');
+// Находим форму в DOM
+const formElement_editProfile = document.querySelector('.popup_type_edit .popup__content .popup__form');
+const formElement_newCard = document.querySelector('.popup_type_new-card .popup__content .popup__form');
+// Находим поля формы в DOM
+const nameInput = formElement_editProfile.querySelector('.popup__input_type_name');
+const jobInput = formElement_editProfile.querySelector('.popup__input_type_description');
+// Находим DOM-элемент заголовок profile__title
+const nameInputContent = document.querySelector('.profile__title');
+// Находим DOM-элемент параграф profile__description
+const jobInputContent = document.querySelector('.profile__description');
 
-// Вывести карточки из массива initialCards на страницу
-initialCards.forEach(element => {
-    placesList.append(addCard(element.link, element.name, deleteCardFunc, likeCardFunc, clickCardImageFunc)); // еще аргументом надо передать ф-ю лайк и клик по img
-});
+//добавим готовый класс в Dom-узлы попапов для плавности их открытия/закрыти
+popupTypeEdit.classList.add('popup_is-animated');
+popupTypeNewCard.classList.add('popup_is-animated');
+popupTypeImage.classList.add('popup_is-animated');
 
-// функция откывающая попап с картинкой карточки, на которой был клик. Она передается в качестве аргумента в ф-цию addCard
+// функция открывающая попап с картинкой карточки, на которой был клик. Она передается в качестве аргумента в ф-цию addCard
 function clickCardImageFunc(evt) { 
-    // popupTypeImage.classList.add('popup_is-opened');
     openModal(popupTypeImage);
     const popImg = popupTypeImage.querySelector('.popup__image');
-    console.log("popImg:");    
-    console.log(popImg);
     popImg.src = evt.target.src;
-    console.log("evt.target:");
-    console.log(evt.target);
-
-    // popupTypeImage.addEventListener('click', removeClassPoppupIsOpened_forImage);
-    // hidePopupByEsc(popupTypeImage);    
 }
 
-
 //добавляем слушателя на кнопку edit
-profileEditButton.addEventListener('click', function () {
+profileEditButton.addEventListener('click', () => {
     // при каждом клике на кнопке редактирования полям инпута присвоить значения со страницы
     nameInput.value = nameInputContent.textContent;
     jobInput.value = jobInputContent.textContent;
-
     openModal(popupTypeEdit);
-    // popupTypeEdit.addEventListener('click', removeClassPoppupIsOpened);
 });
-
 
 //добавляем слушателя на кнопку добавления карточки
-profileAddButton.addEventListener('click', function () {
+profileAddButton.addEventListener('click', () => {
     openModal(popupTypeNewCard);
-    // popupTypeNewCard.addEventListener('click', removeClassPoppupIsOpened_forNewCard);
 });
 
-// до сюда работает КРУТЬ
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+formElement_editProfile.addEventListener('submit', handleFormSubmit_editProfile);
+formElement_newCard.addEventListener('submit', handleFormSubmit_newCard);
 
-
-//обработчик события submit
-// Находим форму в DOM
-const formElement_edit = document.querySelector('.popup_type_edit .popup__content .popup__form');// Воспользуйтесь методом querySelector()
-
-// console.log(formElement);
-// Находим поля формы в DOM
-const nameInput = formElement_edit.querySelector('.popup__input_type_name'); // Воспользуйтесь инструментом .querySelector()
-const jobInput = formElement_edit.querySelector('.popup__input_type_description'); // Воспользуйтесь инструментом .querySelector()
-
-
-const nameInputContent = document.querySelector('.profile__title');
-const jobInputContent = document.querySelector('.profile__description');
-
-
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function handleFormSubmit(evt) {
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+function handleFormSubmit_editProfile(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-    console.log("in handleFormSubmit");
-    console.log(evt.target);
-    if (evt.currentTarget.parentNode.innerHTML.includes('edit-profile')) {
-        nameInputContent.textContent = nameInput.value;
-        jobInputContent.textContent = jobInput.value;
-        // closeModal(evt.target);
-        popupTypeEdit.classList.remove('popup_is-opened');
-    }
+    // Зафиксируем текстовое содержимое элементов заголовка и параграфа из полей формы
+    nameInputContent.textContent = nameInput.value;
+    jobInputContent.textContent = jobInput.value;
+    closeModal(popupTypeEdit);
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement_edit.addEventListener('submit', handleFormSubmit);
-
-//****************/
-
-const formElement_newCard = document.querySelector('.popup_type_new-card .popup__content .popup__form');// Воспользуйтесь методом querySelector()
-
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function handleFormSubmit_newCard(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // console.log(evt.currentTarget['place-name'].value);
-    // console.log(evt.currentTarget.link.value);
     const obj = {
         name: evt.currentTarget['place-name'].value,
         link: evt.currentTarget.link.value
     }
-    
-    placesList.prepend(addCard(obj.link, obj.name, deleteCardFunc, likeCardFunc, clickCardImageFunc)); 
+    placesList.prepend(addCard(obj.link, obj.name, deleteCardFunc, likeCardFunc, clickCardImageFunc));
     evt.currentTarget.reset();
-    popupTypeNewCard.classList.remove('popup_is-opened');
+    closeModal(popupTypeNewCard);
 }
 
-formElement_newCard.addEventListener('submit', handleFormSubmit_newCard);
+// Вывести карточки из массива initialCards на страницу
+initialCards.forEach(element => {
+    placesList.append(addCard(element.link, element.name, deleteCardFunc, likeCardFunc, clickCardImageFunc));
+});
+
 
 
 
